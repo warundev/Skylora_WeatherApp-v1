bde86660aaa29955e5bcbc1d9a99ce1fdfbdf57b
Waruna Liyanapathirana <2020icts03@vau.jfn.ac.lk>
2025-07-12 08:20:38 +0530

    Initial Commit

---
+++ WeatherApp-v1/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  WeatherApp-v1
+//
+//  Created by Waruna Liyanapathirana on 2025-07-12.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ WeatherApp-v1/WeatherApp_v1App.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherApp_v1App.swift
+//  WeatherApp-v1
+//
+//  Created by Waruna Liyanapathirana on 2025-07-12.
+//
+
+import SwiftUI
+
+@main
+struct WeatherApp_v1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
