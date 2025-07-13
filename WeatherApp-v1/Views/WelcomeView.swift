e1426dc9dc70d6895f297fa4cd5f0d01f8a42c9d
Waruna Liyanapathirana <2020icts03@vau.jfn.ac.lk>
2025-07-13 11:19:32 +0530

    day1

---
+++ WeatherApp-v1/Views/WelcomeView.swift
@@ -0,0 +1,18 @@
+//
+//  WelcomeView.swift
+//  WeatherApp-v1
+//
+//  Created by Waruna Liyanapathirana on 2025-07-12.
+//
+
+import SwiftUI
+
+struct WelcomeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    WelcomeView()
+}
