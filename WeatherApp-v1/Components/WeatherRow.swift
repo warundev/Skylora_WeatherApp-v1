73ca2d5d9aa473b5096ca43fcd9631b488891d4a
Waruna Liyanapathirana <2020icts03@vau.jfn.ac.lk>
2025-07-15 00:39:09 +0530

    finiseh

---
+++ WeatherApp-v1/Components/WeatherRow.swift
@@ -0,0 +1,18 @@
+//
+//  WeatherRow.swift
+//  WeatherApp-v1
+//
+//  Created by Waruna Liyanapathirana on 2025-07-15.
+//
+
+import SwiftUI
+
+struct WeatherRow: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    WeatherRow()
+}
